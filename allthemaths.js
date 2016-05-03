cf442a2a366914235a55139cb3f1e659b2390f4e
Mouse Braun <mouse@knoblau.ch>
2016-05-03 20:57:11 +0200

    initial nightmare test config setup

--- allthemaths.js
+++ allthemaths.js
@@ -13,34 +13,56 @@
  * @param {Object} _modules all the modules (including this one)
  * @param {Object} userConfig config options
  */
+const Nightmare     = require( 'nightmare' );
+
+
 module.exports = function Allthemaths( _bot, _modules, userConfig )
 {
     return {
+
+        /**
+         *
+         */
+        toLatex : function( from, to, _text )
+        {
+            const Nightmare     = require( 'nightmare' );
+
+            return new Promise( function( resolve, reject )
+            {
+                var url = 'http://www.sciweavers.org/free-online-latex-equation-editor';
+
+                var nightmare = new Nightmare();
+                nightmare.goto( url )
+                        .type( '#texEqnEditor', _text )
+                        .click( 'input[type=submit].tex2img_iButton' )
+                        .wait( '#iImgLoader > img' )
+                        .evaluate( function()
+                        {
+                            var _i = document.querySelector( '#iImgLoader > img' );
+                            return _i.src;
+                        } )
+                        .then( function( src )
+                        {
+                            resolve( src );
+                        } );
+            } ).then( _r => _r );
+        },
+
 
         /**
          * Responses
          **/
         responses : function( from, to, text, botText )
         {
-            var command = text.slice( 1 ).split( ' ' )[ 0 ];
+            var textSplit   = text.slice( 1 ).split( ' ' );
+            var command     = textSplit[ 0 ];
+            var _text       = textSplit.join( ' ' );
 
             switch ( command )
             {
 
-                case 'dance':
-                    var dancer = Math.floor( Math.random() * 80 );
-                    if ( dancer === 3 )
-                    {
-                        botText = '└[∵┌]└[ ∵ ]┘[┐∵]┘';
-                    }
-                    else
-                    {
-                        botText = '♪┏(・o･)┛♪┗ ( ･o･) ┓♪';
-                    }
-                    break;
-                case 'zzz':
-                case 'sleep':
-                    botText = '(- o - ) zzZ ☽';
+                case 'domath':
+                    botText = toLatex( from, to, _text );
                     break;
             }
 
